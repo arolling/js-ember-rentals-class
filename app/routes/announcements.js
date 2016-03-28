d17d54ead6d95a894e66212c9e026149fd918c1d
Abigail Rolling and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-28 11:06:28 -0700

    Add new announcement adding functionality

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('announcement');
   },
+
+  actions: {
+    save3(params){
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('announcements');
+    }
+  }
 });
